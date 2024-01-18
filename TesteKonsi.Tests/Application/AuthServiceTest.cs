4e33594d90ee6175035adf914ce5a5b24651c411
Yuri Moreira <yurimoreira557@gmail.com>
2024-01-18 14:09:18 -0300

    test: authServiceTest

---
+++ TesteKonsi.Tests/Application/AuthServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using TesteKonsi.Application.DTOs.Request;
+using TesteKonsi.Application.DTOs.Response;
+using TesteKonsi.Domain.Contracts.Infra.Services;
+using TesteKonsi.Domain.UseCases;
+
+namespace TesteKonsi.Tests.Application;
+
+public class AuthServiceTest
+{
+    private readonly Mock<IAuthService> _authService;
+
+    public AuthServiceTest()
+    {
+        _authService = new();
+    }
+    [Fact]
+    public async void AuthReturnSuccess()
+    {
+        var request = new AuthRequestDTO("test@konsi.com.br", "Test@Konsi2023*");
+        
+        var useCase = new AuthUseCase(_authService.Object);
+
+        var result = await useCase.Execute(request);
+        
+        Assert.True(result.Success);
+    }
+}
